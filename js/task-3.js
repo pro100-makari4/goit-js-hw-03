c8c5cb2b86d9ad4526d507b90aefddc407156f74
Vladyslav Makarenko <makarenko.vlad.112@gmail.com>
2023-12-11 23:54:15 +0100

    deleted "join" method from console.log

--- js/task-3.js
+++ js/task-3.js
@@ -4,8 +4,8 @@ function filterArray(numbers, value) {
   return numbers.filter(iterator => iterator > value);
 }
 
-console.log(filterArray([1, 2, 3, 4, 5], 3).join(', ')); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4).join(', ')); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5).join(', ')); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38).join(', ')); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20).join(', ')); // [24, 41, 76]
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
